98f94331423abc62f997581b8978c64f28aaaba2
Muhammet <muhammetsimsek306@gmail.com>
2022-08-29 15:58:49 +0300

    table added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <hello-aykut></hello-aykut>
   <hello-muhammet></hello-muhammet>
+  <table-vue></table-vue>
 </template>
 
 <script>
 import HelloAykut from "./components/AykutsComponents/HelloAykut.vue";
 import HelloMuhammet from "./components/MuhammetsComponents/HelloMuhammet.vue";
+import TableVue from "./components/MuhammetsComponents/TableVue";
 export default {
   name: "App",
   components: {
     HelloAykut,
-    HelloMuhammet
+    HelloMuhammet,
+    TableVue
   },
 };
 </script>
