7a4d52c73437b6056a6145c48a77ded054f8ba3e
Muhammet <muhammetsimsek306@gmail.com>
2022-08-31 11:01:30 +0300

    removed 2 component from MuhammetsComponent

--- src/components/MuhammetsComponents/TableVue.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>Tablo</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam nobis amet ducimus quisquam distinctio facere saepe asperiores, totam possimus cupiditate temporibus, optio eos itaque et aliquid quidem nisi. Nulla, iste!</p>
-</template>
